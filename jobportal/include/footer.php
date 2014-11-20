660865f27bf7fdc39375c422f7218e422d906ba8
hussnainraza <hussnain.raza@incubasys.com>
2014-11-20 15:11:09 +0500

    20-11-14
    responsive issues left

--- jobportal/include/footer.php
+++ jobportal/include/footer.php
@@ -1,11 +1,20 @@
-<footer>
-			<div class="top-header">
-				<div class="container">
-					<?php include('navigation.php');?>
-				</div>
+	<footer id="footer">
+			<div class="container">
+				<strong class="logo">
+					<a href="#"></a>
+				</strong>
+				<nav class="navigation">
+				    <!-- Collect the nav links, forms, and other content for toggling -->
+				      <ul>
+				        <li class="active"><a href="#">home<span class="sr-only">(current)</span></a></li>
+				        <li><a href="#">about</a></li>
+				        <li><a href="#">service</a></li>
+				        <li><a href="#">register</a></li>
+				        <li><a href="#">contact</a></li>
+				      </ul>
+				</nav>
 			</div>
-
-</footer>
+	</footer>
 </div>
 </body>
 </html>
