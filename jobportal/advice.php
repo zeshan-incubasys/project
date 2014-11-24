3dbb5d75503bde1c99364eeb772a067422913029
Khurram <khurram.javed@incubasys.com>
2014-11-24 17:36:03 +0500

    new pages

---
+++ jobportal/advice.php
@@ -0,0 +1,34 @@
+<?php  include('include/header.php');?>
+	<section class="banner">
+		<div class="container">
+			<h1>Consultancy/Advice</h1>
+			<span class="title">We are more than a crew agency </span>
+		</div>
+	</section>
+	<div class="advice container">
+		<p>At Marina ReCrewter, we are more than a crew agency. Due to our immense platform of (local) network and contacts and maritime background, we have received requests to assist and support in a more consultative role, acting as initiating link with management related tasks.</p>
+		<ul>
+			<li>ISM code, Lloyds certified Internal Management System Auditor </li>
+			<li>ISPS code, Lloyds certified Company and Ship Security Officer, CSO/SSO</li>
+			<li>Classification and Statutory Surveys, Lloyds certified</li>
+			<li>Risk Management and Incident Investigation, Lloyds certified</li>
+			<li>We are well versed in Port State Controls, Class and Flag State Inspections, MCA USCG rules and regulation. Pre-audits: Crew safety and Security training. </li>
+			<li>Maintenance and repair, sea-trials and delivery crew </li>
+		</ul>
+	</div>
+	<div class="image-section container">
+		<div class="col-md-5 row">
+			<img src="images/advice1.jpg" alt="">
+		</div>
+		<div class="col-md-7 row">
+			<img src="images/advice2.jpg" alt="">
+			<a href="#" class="btn">ENQUIRE NOW</a>
+		</div>
+	</div>
+	<section class="what-do">
+        <div class="container head">
+          <h2><span>Candidates – Work &amp; Live Your Dream!</span></h2>
+          <p>Whether you are an experienced candidate or a new talent, it is our goal to help advance your career and hopefully contribute to securing your next job. We are committed to assist and advice. You will be supported by our hands-on approach, an honest assessment matching realistic opportunities.</p>
+        </div>
+	</section>
+<?php include('include/footer.php');?>
