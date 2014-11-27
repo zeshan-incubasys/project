f665effcb14b873181b984d013190b955cefba97
hussnainraza <hussnain.raza@incubasys.com>
2014-11-27 11:29:07 +0500

    index page completed .

--- gamezone/include/footer.php
+++ gamezone/include/footer.php
@@ -1,5 +1,20 @@
 	<footer id="footer">
 		
 	</footer>
+	<script type="text/javascript" src="<?=JS_URL?>masonry.pkgd.min.js"></script>
+	<script type="text/javascript">
+		var container = document.querySelector('#hero-masonry');
+		var msnry = new Masonry( container, {
+		  // options
+		  columnWidth: 5,
+		  itemSelector: '.game-wrap',
+		  isAnimated: true		
+		});
+		$('document').ready(function(){
+			$('window').load(function(){
+				msnry.layout();
+			});
+		});
+	</script>
 </body>
 </html>
--- gamezone/index.php
+++ gamezone/index.php
@@ -1,5 +1,197 @@
 <?php  include('include/header.php');?>
-<div class="container">
-	
+<div id="main">
+
+
+	<div id="sidebar" class="col-sm-3 col-md-2">
+		<div class="open-close">
+			<a class="opener" href="">menu</a>
+			<div class="slide">
+				<div class="widget">
+					<strong class="heading">Browse</strong>
+					<ul>
+						<li><a href="javascript:void(0)">Recent Games</a></li>
+						<li class="chart"><a href="javascript:void(0)">Top Charts</a></li>
+						<li class="release"><a href="javascript:void(0)">New Releases</a></li>
+					</ul>
+				</div>
+				<div class="widget">
+					<strong class="heading">Your GAMES</strong>
+					<ul>
+						<li class="games"><a href="javascript:void(0)">My Games</a></li>
+						<li class="history"><a href="javascript:void(0)">History</a></li>
+					</ul>
+				</div>
+				<div class="widget option">
+					<strong class="heading">More Options</strong>
+					<ul>
+						<li class="settings"><a href="javascript:void(0)">Settings</a></li>
+						<li class="out"><a href="javascript:void(0)">Log Out</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<div id="content" class="col-sm-9 col-md-10">
+		<div class="header">
+			<div class="user-area">
+				<div class="numbers">
+					<span class="block"><span>vs</span>1000</span>
+					<span class="block"><span>rs</span>13000</span>
+				</div>
+				<div class="user-details">
+					<div class="image">
+						<img src="<?php echo IMAGE_URL; ?>/img-dp.png" alt="">
+					</div>
+					<div class="details">
+						<h1>AbuBakar Butt</h1>
+						<span>Lahore, Pakistan, Punjab</span>
+					</div>
+				</div>
+			</div>
+			<div class="info-area">
+				<div class="links-area">
+					<span class="active"><a href="javascript:void(0)">All Games</a></span>
+					<span><a href="javascript:void(0)">Installed Games</a></span>
+				</div>
+				<div class="info">
+					<span>22 Games Played</span>
+					<span>12 Active Competitions</span>
+				</div>
+			</div>
+		</div>
+		<div class="content-heading">
+			<h2>Top Games</h2>
+		</div>
+		<div id="hero">
+		<section id="hero-masonry" class="content-holder">
+			<div class="game-wrap">
+				<div class="image-wrap">
+					<a href="javascript:void(0)"><img src="<?php echo IMAGE_URL; ?>img1.jpg" alt=""></a>
+				</div>
+				<div class="text-wrap">
+					<strong class="game-title"><a href="javascript:void(0)">Subway Surfers</a></strong>
+					<span class="game-date"><a href="javascript:void(0)">Kiloo</a> - November 6, 2014</span>
+					<span class="game-downloads"><span class="counter">53,98,37,590</span> Downloads</span>
+				</div> 
+			</div>
+
+			<div class="game-wrap">
+				<div class="image-wrap">
+					<a href="javascript:void(0)"><img src="<?php echo IMAGE_URL; ?>img2.jpg" alt=""></a>
+				</div>
+				<div class="text-wrap">
+					<strong class="game-title"><a href="#">Subway Surfers</a></strong>
+					<span class="game-date"><a href="#">Kiloo</a> - November 6, 2014</span>
+					<span class="game-downloads"><span class="counter">53,98,37,590</span> Downloads</span>
+				</div> 
+			</div>
+
+			<div class="game-wrap">
+				<div class="image-wrap">
+					<a href="#"><img src="<?php echo IMAGE_URL; ?>img3.jpg" alt=""></a>
+				</div>
+				<div class="text-wrap">
+					<strong class="game-title"><a href="#">Subway Surfers</a></strong>
+					<span class="game-date"><a href="#">Kiloo</a> - November 6, 2014</span>
+					<span class="game-downloads"><span class="counter">53,98,37,590</span> Downloads</span>
+				</div> 
+			</div>
+
+			<div class="game-wrap">
+				<div class="image-wrap">
+					<a href="#"><img src="<?php echo IMAGE_URL; ?>img4.jpg" alt=""></a>
+				</div>
+				<div class="text-wrap">
+					<strong class="game-title"><a href="#">Subway Surfers</a></strong>
+					<span class="game-date"><a href="#">Kiloo</a> - November 6, 2014</span>
+					<span class="game-downloads"><span class="counter">53,98,37,590</span> Downloads</span>
+				</div> 
+			</div>
+
+			<div class="game-wrap">
+				<div class="image-wrap">
+					<a href="#"><img src="<?php echo IMAGE_URL; ?>img5.jpg" alt=""></a>
+				</div>
+				<div class="text-wrap">
+					<strong class="game-title"><a href="#">Subway Surfers</a></strong>
+					<span class="game-date"><a href="#">Kiloo</a> - November 6, 2014</span>
+					<span class="game-downloads"><span class="counter">53,98,37,590</span> Downloads</span>
+				</div> 
+			</div>
+
+			<div class="game-wrap">
+				<div class="image-wrap">
+					<a href="#"><img src="<?php echo IMAGE_URL; ?>img6.jpg" alt=""></a>
+				</div>
+				<div class="text-wrap">
+					<strong class="game-title"><a href="#">Subway Surfers</a></strong>
+					<span class="game-date"><a href="#">Kiloo</a> - November 6, 2014</span>
+					<span class="game-downloads"><span class="counter">53,98,37,590</span> Downloads</span>
+				</div> 
+			</div>
+
+			<div class="game-wrap">
+				<div class="image-wrap">
+					<a href="#"><img src="<?php echo IMAGE_URL; ?>img7.jpg" alt=""></a>
+				</div>
+				<div class="text-wrap">
+					<strong class="game-title"><a href="#">Subway Surfers</a></strong>
+					<span class="game-date"><a href="#">Kiloo</a> - November 6, 2014</span>
+					<span class="game-downloads"><span class="counter">53,98,37,590</span> Downloads</span>
+				</div> 
+			</div>
+
+			<div class="game-wrap">
+				<div class="image-wrap">
+					<a href="#"><img src="<?php echo IMAGE_URL; ?>img8.jpg" alt=""></a>
+				</div>
+				<div class="text-wrap">
+					<strong class="game-title"><a href="#">Subway Surfers</a></strong>
+					<span class="game-date"><a href="#">Kiloo</a> - November 6, 2014</span>
+					<span class="game-downloads"><span class="counter">53,98,37,590</span> Downloads</span>
+				</div> 
+			</div>
+
+			<div class="game-wrap">
+				<div class="image-wrap">
+					<a href="#"><img src="<?php echo IMAGE_URL; ?>img9.jpg" alt=""></a>
+				</div>
+				<div class="text-wrap">
+					<strong class="game-title"><a href="#">Subway Surfers</a></strong>
+					<span class="game-date"><a href="#">Kiloo</a> - November 6, 2014</span>
+					<span class="game-downloads"><span class="counter">53,98,37,590</span> Downloads</span>
+				</div> 
+			</div>
+
+			<div class="game-wrap">
+				<div class="image-wrap">
+					<a href="#"><img src="<?php echo IMAGE_URL; ?>img10.jpg" alt=""></a>
+				</div>
+				<div class="text-wrap">
+					<strong class="game-title"><a href="#">Subway Surfers</a></strong>
+					<span class="game-date"><a href="#">Kiloo</a> - November 6, 2014</span>
+					<span class="game-downloads"><span class="counter">53,98,37,590</span> Downloads</span>
+				</div> 
+			</div>
+
+			<div class="game-wrap">
+				<div class="image-wrap">
+					<a href="#"><img src="<?php echo IMAGE_URL; ?>img1.jpg" alt=""></a>
+				</div>
+				<div class="text-wrap">
+					<strong class="game-title"><a href="#">Subway Surfers</a></strong>
+					<span class="game-date"><a href="#">Kiloo</a> - November 6, 2014</span>
+					<span class="game-downloads"><span class="counter">53,98,37,590</span> Downloads</span>
+				</div> 
+			</div>
+		</section>
+	</div>
+
+
+	</div>
 </div>
+
 <?php include('include/footer.php');?>
