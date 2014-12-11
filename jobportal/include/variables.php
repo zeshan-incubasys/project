58ae7064aaa75f268d09247b6ac7449d06e91277
Khurram <khurram.javed@incubasys.com>
2014-12-11 12:33:40 +0500

    var

--- jobportal/include/variables.php
+++ jobportal/include/variables.php
@@ -2,11 +2,11 @@
 if($_SERVER['HTTP_HOST']=='localhost'||$_SERVER['SERVER_NAME']=='localhost')
 {
     $project_name="jobportal";
-    define('BASE_URL',"http://localhost/project-office/$project_name/");
+    define('BASE_URL',"http://localhost/project/$project_name/");
 }else
 {
 	$project_name="jobportal";
-    define('BASE_URL',"http://192.168.0.3/project-office/$project_name/");// http://sitename.com/
+    define('BASE_URL',"http://192.168.0.3/project/$project_name/");// http://sitename.com/
 }
 define('CSS_URL',BASE_URL."css/");
 define('IMAGE_URL',BASE_URL."images/");
