57a15083b1b98433b559903570ade0c29af33973
Khurram <khurram.javed@incubasys.com>
2014-12-16 18:15:43 +0500

    svg image

---
+++ dfp/packages.php
@@ -0,0 +1,43 @@
+<?php  include('include/header.php');?>
+<div id="main">
+	<strong class="title-txt">Galler.ee is Design For Photographers. Choose a membership plan that fits your needs and budget to start growing your photography business.</strong>
+	<section class="packages">
+		<div class="col">
+			<strong class="pkg-title">Basic</strong>
+			<span class="price">$0</span>
+			<ul>
+				<li>Try our free items</li>
+				<li>Pay as you download
+				<li>Easily upgrade later</li>
+				<li>Weekly product updates</li>
+				<li>No commitment</li>
+			</ul>
+			<a href="#" class="btn">I want to try DFP</a>
+		</div>
+		<div class="col odd">
+			<strong class="pkg-title"><span>Most Popular</span>Monthly</strong>
+			<span class="price">$20<span> / mo</span></span>
+			<ul>
+				<li class="help">Unlimited downloads <a href="#">?</a></li>
+				<li>Pay month to month</li>
+				<li>Request the products you want</li>
+				<li>Weekly product updates</li>
+				<li class="help">12 month commitment <a href="#">?</a></li>
+			</ul>
+			<a href="#" class="btn">I want to pay monthly</a>
+		</div>
+		<div class="col">
+			<strong class="pkg-title">Annual</strong>
+			<span class="price">$220<span> / yr</span></span>
+			<ul>
+				<li class="help">Unlimited downloads <a href="#">?</a></li>
+				<li>1 full year of membership</li>
+				<li>Request the products you want</li>
+				<li>Weekly product updates</li>
+				<li>1 month free</li>
+			</ul>
+			<a href="#" class="btn">I want 1 month free</a>
+		</div>
+	</section>
+</div>
+<?php include('include/footer.php');?>
