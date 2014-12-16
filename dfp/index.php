1ba9505e804335bdf5a6b930e14ac7885c9c613a
Zeshan Hassan <zeshan.hassan1989@gmail.com>
2014-12-16 15:18:42 +0500

    added new project dfp

---
+++ dfp/index.php
@@ -0,0 +1,190 @@
+<?php  include('include/header.php');?>
+<section class="slider">
+	<img src="<?=IMAGE_URL?>img1.jpg" alt="">
+	<div class="desc">
+		<div class="holder">
+			<div class="content">
+				<h1>try our products free with a basic account</h1>
+				<form class="basic-form">
+					<fieldset>
+						<div class="row">
+							<input type="email" class="email" placeholder="you@yourname.com">
+						</div>
+						<div class="row">
+							<input type="password" placeholder="password">
+							<input type="password" placeholder="confirm password">
+							<input type="submit" value="Get free designs">
+						</div>
+					</fieldset>
+				</form>
+			</div>
+		</div>
+	</div>
+</section>
+<div id="main">
+	<div class="products">
+		<div class="head">
+			<h2>picks of the season</h2>
+			<div class="paged">
+				<div class="pages">
+					<span class="cur-num"></span>
+					<span>/</span>
+					<span class="all-num"></span>
+				</div>
+				<div class="btns">
+					<a class="btn-prev" href="#">Previous</a>
+					<a class="btn-next" href="#">Next</a>
+				</div>
+			</div>
+			<div class="pagination">
+			</div>
+		</div>
+		<div class="mask">
+			<div class="slideset">
+				<div class="slide">
+					<img src="<?=IMAGE_URL?>img-prod.jpg" alt="">
+					<div class="details">
+						<div class="title-area">
+							<strong class="title">Chairs</strong>
+							<span class="by">by Filicio Ernrim</span>
+						</div>
+						<a href="#" class="ico-cart">Add<br>to cart</a>
+					</div>
+				</div>
+				<div class="slide">
+					<img src="<?=IMAGE_URL?>img-prod.jpg" alt="">
+					<div class="details">
+						<div class="title-area">
+							<strong class="title">Chairs</strong>
+							<span class="by">by Filicio Ernrim</span>
+						</div>
+						<a href="#" class="ico-cart">Add<br>to cart</a>
+					</div>
+				</div>
+				<div class="slide">
+					<img src="<?=IMAGE_URL?>img-prod.jpg" alt="">
+					<div class="details">
+						<div class="title-area">
+							<strong class="title">Chairs</strong>
+							<span class="by">by Filicio Ernrim</span>
+						</div>
+						<a href="#" class="ico-cart">Add<br>to cart</a>
+					</div>
+				</div>
+				<div class="slide">
+					<img src="<?=IMAGE_URL?>img-prod.jpg" alt="">
+					<div class="details">
+						<div class="title-area">
+							<strong class="title">Chairs</strong>
+							<span class="by">by Filicio Ernrim</span>
+						</div>
+						<a href="#" class="ico-cart">Add<br>to cart</a>
+					</div>
+				</div>
+				<div class="slide">
+					<img src="<?=IMAGE_URL?>img-prod.jpg" alt="">
+					<div class="details">
+						<div class="title-area">
+							<strong class="title">Chairs</strong>
+							<span class="by">by Filicio Ernrim</span>
+						</div>
+						<a href="#" class="ico-cart">Add<br>to cart</a>
+					</div>
+				</div>
+				<div class="slide">
+					<img src="<?=IMAGE_URL?>img-prod.jpg" alt="">
+					<div class="details">
+						<div class="title-area">
+							<strong class="title">Chairs</strong>
+							<span class="by">by Filicio Ernrim</span>
+						</div>
+						<a href="#" class="ico-cart">Add<br>to cart</a>
+					</div>
+				</div>
+				<div class="slide">
+					<img src="<?=IMAGE_URL?>img-prod.jpg" alt="">
+					<div class="details">
+						<div class="title-area">
+							<strong class="title">Chairs</strong>
+							<span class="by">by Filicio Ernrim</span>
+						</div>
+						<a href="#" class="ico-cart">Add<br>to cart</a>
+					</div>
+				</div>
+				<div class="slide">
+					<img src="<?=IMAGE_URL?>img-prod.jpg" alt="">
+					<div class="details">
+						<div class="title-area">
+							<strong class="title">Chairs</strong>
+							<span class="by">by Filicio Ernrim</span>
+						</div>
+						<a href="#" class="ico-cart">Add<br>to cart</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="products">
+		<div class="head">
+			<h2>picks of the season</h2>
+			<div class="paged">
+				<div class="pages">
+					<span class="cur-num"></span>
+					<span>/</span>
+					<span class="all-num"></span>
+				</div>
+				<div class="btns">
+					<a class="btn-prev" href="#">Previous</a>
+					<a class="btn-next" href="#">Next</a>
+				</div>
+			</div>
+			<div class="pagination">
+			</div>
+		</div>
+		<div class="mask">
+			<div class="slideset">
+				<div class="slide">
+					<img src="<?=IMAGE_URL?>img-prod.jpg" alt="">
+					<div class="details">
+						<div class="title-area">
+							<strong class="title">Chairs</strong>
+							<span class="by">by Filicio Ernrim</span>
+						</div>
+						<a href="#" class="ico-cart">Add<br>to cart</a>
+					</div>
+				</div>
+				<div class="slide">
+					<img src="<?=IMAGE_URL?>img-prod.jpg" alt="">
+					<div class="details">
+						<div class="title-area">
+							<strong class="title">Chairs</strong>
+							<span class="by">by Filicio Ernrim</span>
+						</div>
+						<a href="#" class="ico-cart">Add<br>to cart</a>
+					</div>
+				</div>
+				<div class="slide">
+					<img src="<?=IMAGE_URL?>img-prod.jpg" alt="">
+					<div class="details">
+						<div class="title-area">
+							<strong class="title">Chairs</strong>
+							<span class="by">by Filicio Ernrim</span>
+						</div>
+						<a href="#" class="ico-cart">Add<br>to cart</a>
+					</div>
+				</div>
+				<div class="slide">
+					<img src="<?=IMAGE_URL?>img-prod.jpg" alt="">
+					<div class="details">
+						<div class="title-area">
+							<strong class="title">Chairs</strong>
+							<span class="by">by Filicio Ernrim</span>
+						</div>
+						<a href="#" class="ico-cart">Add<br>to cart</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php include('include/footer.php');?>
