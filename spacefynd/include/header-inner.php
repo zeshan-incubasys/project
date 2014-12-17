7faf1141ef0a6df6bf759e18c1f92d449b5ed18c
Khurram <khurram.javed@incubasys.com>
2014-12-17 15:29:20 +0500

    changes

--- spacefynd/include/header-inner.php
+++ spacefynd/include/header-inner.php
@@ -1,6 +1,34 @@
 <?php include('variables.php');?>
 <?php include('head.php');?>
 <body class="inner">
-<header>
-	
-</header>
+	<header>
+		<div class="top-bar">
+			<div class="logo-area">
+				<strong class="logo"><a href="#">spacefynd</a></strong>
+				<form class="loc-form" action="#">
+					<fieldset>
+						<input type="text" placeholder="Where are you going?">
+					</fieldset>
+				</form>
+				<ul class="small-menu">
+					<li>
+						<a href="#">Browse</a>
+						<div class="drop">
+							<ul>
+								<li><a href="#">Popular</a></li>
+								<li><a href="#">Recent</a></li>
+								<li><a href="#">Neighborhood</a></li>
+							</ul>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="right-area">
+				<span class="pic"></span>
+				<a href="#" class="profile">Abubakar</a>
+				<a href="#" class="email">Email</a>
+				<a href="#" class="help">Help</a>
+				<a href="#" class="btn">List Your Space</a>
+			</div>
+		</div>
+	</header>
