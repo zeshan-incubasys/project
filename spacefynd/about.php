ceea8bb3ad63c3d9db3fb424058c0b5c957056c3
Khurram <khurram.javed@incubasys.com>
2014-12-17 18:09:21 +0500

    day 1

--- spacefynd/about.php
+++ spacefynd/about.php
@@ -1,5 +1,16 @@
 <?php  include('include/header-inner.php');?>
 <div id="main">
-	
+	<aside id="sidebar">
+		<ul class="sidenav">
+			<li class="active"><a href="#">About Us</a></li>
+			<li><a href="#">Founders</a></li>
+		</ul>
+	</aside>
+	<section id="content">
+		<h1>About Us</h1>
+		<p>spacefynd helps you find and book exciting venues for free. Its proud tradition of supporting unheralded spaces ensures unparalleled access to new and exciting venues. Hiring a memorable space is easier than ever before.spacefynd helps you find and book exciting venues for free. Its proud tradition of supporting unheralded spaces ensures unparalleled access to new and exciting venues. Hiring a memorable space is easier than ever before.spacefynd helps you find and book exciting venues for free. </p>
+		<p>Its proud tradition of supporting unheralded spaces ensures unparalleled access to new and exciting venues. Hiring a memorable space is easier than ever before.spacefynd helps you find and book exciting venues for free. Its proud tradition of supporting unheralded spaces ensures unparalleled access to new and exciting venues. Hiring a memorable space is easier than ever before.</p>
+		<img src="<?php echo IMAGE_URL; ?>img-map.png" alt="" class="map">
+	</section>
 </div>
 <?php include('include/footer.php');?>
