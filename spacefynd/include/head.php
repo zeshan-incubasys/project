01e63f75d6ddd482ba2e3f8e1f221c277bae0663
Zeshan Hassan <zeshan.hassan1989@gmail.com>
2014-12-17 11:18:42 +0500

    initial commit

---
+++ spacefynd/include/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Index Page</title>
+    <link rel="stylesheet" href="<?=CSS_URL?>bootstrap.min.css" media="all" type="text/css">
+    <link rel="stylesheet" href="<?=CSS_URL?>customize.css" media="all" type="text/css">
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script type="text/javascript">window.jQuery || document.write('<script src="<?=JS_URL?>jquery-1.11.1.min.js"><\/script>')</script>
+    <script type="text/javascript" src="<?=JS_URL?>bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?=JS_URL?>customize.js"></script>
+</head>
