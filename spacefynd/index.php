301d463bd23f736dabff0949ec703b5df0ce8cf1
Zeshan Hassan <zeshan.hassan1989@gmail.com>
2014-12-17 18:09:49 +0500

    rope section

--- spacefynd/index.php
+++ spacefynd/index.php
@@ -1,4 +1,44 @@
 <?php  include('include/header.php');?>
+<section id="how-it-works">
+    <div class="heading">
+        <h1>
+            Curated Shortlists
+            <small>Looking for some inspiration? We've created shortlists of the top venues.</small>
+        </h1>
+    </div>
+    <div>
+        <div class="pull-left">
+            <div>
+                <img src="" alt=""/>
+                <div class="desc"></div>
+            </div>
+            <div>
+                <img src="" alt=""/>
+                <div class="desc"></div>
+            </div>
+            <div>
+                <img src="" alt=""/>
+                <div class="desc"></div>
+            </div>
+        </div>
+        <div class="pull-right">
+            <div>
+                <img src="" alt=""/>
+                <div class="desc"></div>
+            </div>
+            <div>
+                <img src="" alt=""/>
+                <div class="desc"></div>
+            </div>
+            <div>
+                <img src="" alt=""/>
+                <div class="desc"></div>
+            </div>
+        </div>
+        <img src="<?=IMAGE_URL?>" alt="" />
+    </div>
+
+</section>
 
 
 
