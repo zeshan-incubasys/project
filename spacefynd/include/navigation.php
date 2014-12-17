dc8c5be7f1d82987a309d221c4b59bce589a5c23
Zeshan Hassan <zeshan.hassan1989@gmail.com>
2014-12-17 15:29:17 +0500

    minor changes

--- spacefynd/include/navigation.php
+++ spacefynd/include/navigation.php
@@ -1,5 +1,16 @@
-<nav id="nav">
-    <ul>
-        <li><a href="#">Home</a></li>
-    </ul>
-</nav>
+<div class="head-container">
+    <nav id="nav">
+        <a href="javscript:void(0)" class="btn btn-sky">List Your Space</a>
+        <ul>
+            <li class="dropdown"><a href="#">Signup</a></li>
+            <li class="dropdown"><a href="#">Login</a></li>
+            <li><a href="#">Help</a></li>
+        </ul>
+
+    </nav>
+    <div class="">
+        <div class=""></div>
+        <div class=""></div>
+    </div>
+</div>
+
