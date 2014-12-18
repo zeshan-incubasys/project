fe6f00bf3591f952dab7df3f90b194caf04c1f78
Zeshan Hassan <zeshan.hassan1989@gmail.com>
2014-12-18 17:01:04 +0500

    footer added

--- spacefynd/include/footer.php
+++ spacefynd/include/footer.php
@@ -1,5 +1,28 @@
 <footer>
-
+    <div class="container">
+        <div class="footer-left-col">
+            <img src="<?=IMAGE_URL?>logo.svg" alt="logo" class="footer-logo"/>
+            <h3>Follow Us</h3>
+        </div>
+        <div class="footer-right-col">
+            <h4>About SpaceFynd</h4>
+            <p>Whether it’s a wedding celebration, design event, private party or corporate off-site, Spacefynd helps people find the perfect space to host their event with unprecedented ease and convenience. Individuals and companies can also list their spaces in no time, and Spacefynd will match them with customers and help them make get their space hired. 
+            </p>
+        </div>
+        <div class="footer-middle-col">
+            <ul>
+                <li><a href="<?=BASE_URL?>about.php">About</a></li>
+                <li><a href="<?=BASE_URL?>contact.php"> Contact us</a></li>
+                <li><a href="<?=BASE_URL?>"> Venue login</a></li>
+                <li><a href="<?=BASE_URL?>lists.php">List Your Venue</a></li>
+                <li><a href="<?=BASE_URL?>">How it works</a></li>
+            </ul>
+            <div class="footer-link">
+                <a href="javascript:void(0)">contact@spacefynd.com</a>
+                <a href="http://incubasys.com">Designed and Developed by Incubasys</a>
+            </div>
+        </div>
+    </div>
 </footer>
 </body>
 </html>
