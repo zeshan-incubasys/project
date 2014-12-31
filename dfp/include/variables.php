88972b788491247f9da88a6cedeca3e3b8653abd
Zeshan Hassan <zeshan.hassan@incubasys.com>
2014-12-31 14:33:47 +0500

    dfp

--- dfp/include/variables.php
+++ dfp/include/variables.php
@@ -2,7 +2,7 @@
 if($_SERVER['HTTP_HOST']=='localhost'||$_SERVER['SERVER_NAME']=='localhost')
 {
     $project_name="dfp";
-    define('BASE_URL',"http://localhost/$project_name/");
+    define('BASE_URL',"http://localhost/project/$project_name/");
 }else
 {
     define('BASE_URL','http://incubasys.com/');// http://sitename.com/
