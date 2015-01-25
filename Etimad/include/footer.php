70a73dbab88846537aaed2695f6444f095b49e57
Zeshan Hassan <zeshan.hassan@incubasys.com>
2015-01-25 16:36:56 +0500

    header and footer change

--- Etimad/include/footer.php
+++ Etimad/include/footer.php
@@ -1,14 +1,24 @@
+			<div class="footer-banner">
+				  <div class="text-holder">
+						<h1>About Etimad</h1>
+						<p>Etimad Holding is a leading UAE company combining and governing key operating companies and skills that together provide an end-to-end fulfillment, control and assurance for any type of national security model.  Our mission-purpose continues to be providing key solutions for our nations-security.</p>
+						<img src="<?=IMAGE_URL?>tick.png" alt=""/>
+				  </div>
+			</div>
 	</div>
 </div>
+
 	<footer id="footer">
-		<div class="links">
-			<ul>
-				<li><a href="#">Privacy Policy</a></li>
-				<li><a href="#">Terms of Use</a></li>
-				<li><a href="#">Back to Service Selection</a></li>
-			</ul>
-		</div>
-		<span class="copy">&copy; 2014 <a href="#">Etimad</a> All Rights Reserved.</span>
+		  <div class="text-holder">
+			  <div class="links">
+				  <ul>
+					  <li><a href="#">Privacy Policy</a></li>
+					  <li><a href="#">Terms of Use</a></li>
+					  <li><a href="#">Back to Service Selection</a></li>
+				  </ul>
+			  </div>
+			  <span class="copy">&copy; <?=date("Y")?> <a href="#">Etimad</a> All Rights Reserved.</span>
+		  </div>
 	</footer>
 </body>
 </html>
--- Etimad/include/header.php
+++ Etimad/include/header.php
@@ -5,42 +5,44 @@
 		<div id="w2">
 			<header id="header">
 				<div class="holder">
-					<strong class="logo"><a href="<?=BASE_URL?>">etimad</a></strong>
-					<div class="open-close">
-						<a class="opener" href="#"></a>
-						<div id="navigation">
-							<ul>
-								<li class="<?=(strpos($_SERVER['PHP_SELF'], 'index.php') != false)?"active":""?>"><a href="<?=BASE_URL?>">home</a></li>
-								<li class="<?=(strpos($_SERVER['PHP_SELF'], 'etimad-governance.php') != false)?"active":""?>">
-									<a href="etimad-governance.php">Etimad Governance</a>
-									<ul class="dropdown">
-										<li><a href="#">Project Risk Management</a></li>
-										<li><a href="#">Fulfillment Management</a></li>
-										<li><a href="#">Compliance Monitoring</a></li>
-										<li><a href="#">Process Efficiencies</a></li>
-										<li><a href="#">Ceritifications</a></li>
-										<li><a href="#">Continuity Planning</a></li>
-										<li><a href="#">Project Risk Management</a></li>
-									</ul>
-								</li>
-								<li class="<?=(strpos($_SERVER['PHP_SELF'], 'our-companies.php') != false)?"active":""?>"><a href="<?=BASE_URL?>our-companies.php">Our companies</a></li>
-								<li class="">
-									<a href="#">solutions</a>
-									<ul class="dropdown">
-										<li><a href="#">Project Risk Management</a></li>
-										<li><a href="#">Fulfillment Management</a></li>
-										<li><a href="#">Compliance Monitoring</a></li>
-										<li><a href="#">Process Efficiencies</a></li>
-										<li><a href="#">Ceritifications</a></li>
-										<li><a href="#">Continuity Planning</a></li>
-										<li><a href="#">Project Risk Management</a></li>
-									</ul>
-								</li>
-								<li><a href="#">Partners</a></li>
-								<li><a href="#">Contact</a></li>
-								<li><a href="#">News</a></li>
-							</ul>
-						</div>
+					  <div class="text-holder">
+						  <strong class="logo"><a href="<?=BASE_URL?>">etimad</a></strong>
+						  <div class="open-close">
+							  <a class="opener" href="#"></a>
+							  <div id="navigation">
+								  <ul>
+									  <li class="<?=(strpos($_SERVER['PHP_SELF'], 'index.php') != false)?"active":""?>"><a href="<?=BASE_URL?>">home</a></li>
+									  <li class="<?=(strpos($_SERVER['PHP_SELF'], 'etimad-governance.php') != false)?"active":""?>">
+										  <a href="etimad-governance.php">Etimad Governance</a>
+										  <ul class="dropdown">
+											  <li><a href="#">Project Risk Management</a></li>
+											  <li><a href="#">Fulfillment Management</a></li>
+											  <li><a href="#">Compliance Monitoring</a></li>
+											  <li><a href="#">Process Efficiencies</a></li>
+											  <li><a href="#">Ceritifications</a></li>
+											  <li><a href="#">Continuity Planning</a></li>
+											  <li><a href="#">Project Risk Management</a></li>
+										  </ul>
+									  </li>
+									  <li class="<?=(strpos($_SERVER['PHP_SELF'], 'our-companies.php') != false)?"active":""?>"><a href="<?=BASE_URL?>our-companies.php">Our companies</a></li>
+									  <li class="">
+										  <a href="#">solutions</a>
+										  <ul class="dropdown">
+											  <li><a href="#">Project Risk Management</a></li>
+											  <li><a href="#">Fulfillment Management</a></li>
+											  <li><a href="#">Compliance Monitoring</a></li>
+											  <li><a href="#">Process Efficiencies</a></li>
+											  <li><a href="#">Ceritifications</a></li>
+											  <li><a href="#">Continuity Planning</a></li>
+											  <li><a href="#">Project Risk Management</a></li>
+										  </ul>
+									  </li>
+									  <li><a href="#">Partners</a></li>
+									  <li><a href="#">Contact</a></li>
+									  <li><a href="#">News</a></li>
+								  </ul>
+							  </div>
+						  </div>
 					</div>
 					
 				</div>
